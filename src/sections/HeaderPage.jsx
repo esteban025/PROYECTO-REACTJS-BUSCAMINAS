d5264c1e4322a64cbefead51949e9c6d999c6db7
EstebanDev <guachichullcaesteban@gmail.com>
2025-12-03 12:03:09 -0500

    Ruta de img

--- src/sections/HeaderPage.jsx
+++ src/sections/HeaderPage.jsx
@@ -1,8 +1,9 @@
+import minaImg from '../assets/mina.webp'
 export const HeaderPage = () => {
   return(
     <header className="space-y-2 p-4 py-6">
       <h1 className="font-semibold text-center flex justify-center items-center gap-4 principal-title">
-        <img className="w-9" src="../src/assets/mina.webp" alt="Imagen de mina" />
+        <img className="w-9" src={minaImg} alt="Imagen de mina" />
         <span className="bg-linear-to-t to-violet-50 from-violet-700 bg-clip-text text-transparent">Buscaminas</span>
         </h1>
       <p className="text-center paragraph-text">¡Bienvenido! Encuentra todas las minas sin detonar ninguna.</p>
